df4d846536196165da99b24826fb07f8a17313ed
AhmedQeshta <ahmedqeshta1999@gmail.com>
2022-12-21 00:11:35 +0200

    use api

--- utils/graphQL/config.ts
+++ utils/graphQL/config.ts
@@ -2,9 +2,6 @@ import { GraphQLClient } from 'graphql-request';
 
 const CONTENT_API: string = process?.env?.NEXT_PUBLIC_GRAPH_CMS_API!;
 
-const graphQLClient = new GraphQLClient(
-  'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbuxgpkr388001ukhel68kta/master',
-);
-// const graphQLClient = new GraphQLClient(CONTENT_API);
+const graphQLClient = new GraphQLClient(CONTENT_API);
 
 export default graphQLClient;
